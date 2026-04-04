823f7110d9365cd438b3df7041a19e9e8da8ba65
hirin <143975933+chohyerinn@users.noreply.github.com>
2026-04-05 02:56:45 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 292 ms, Memory: 54216 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.py
@@ -0,0 +1,13 @@
+import sys
+
+n = int(sys.stdin.readline())
+points = []
+
+for _ in range(n):
+    x, y = map(int, sys.stdin.readline().split())
+    points.append((x, y))
+
+points.sort(key=lambda p: (p[1], p[0]))
+
+for x, y in points:
+    print(x, y)
